b4f8f792ff7915d20a649c1282485d0eb233a5cf
caues <caues@SCHMITZNOT>
2025-04-08 20:54:10 -0300

    Add project files.

---
+++ Diamante - X/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Diamante___X
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
